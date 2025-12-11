056e1897d5af1e186c98f0cda2da332edacccf50
JVP008 <jayeshpatil20081234@gmail.com>
2025-12-11 18:54:33 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Add this if needed for OpenCV
+        maven { url = uri("https://jitpack.io") }
+        flatDir { dirs("app/libs") }
+    }
+}
+
+rootProject.name = "smart_attendance"
+include(":app")
+
